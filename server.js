607f73d240e5598ec6d86947a41ab2294841b324
Sabah <sabahalmel7@gmail.com>
2023-12-06 16:55:20 +0200

    start

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-dotenv.config();
-
 const app = express();
-app.use(cors());
-app.listen(process.env.PORT, () => {
+dotenv.config();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+import { Server } from "socket.io";
+import { createServer } from "http";
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer);
+io.on("connection", (socket) => {
+  socket.emit("connect", { message: "a new client" });
+  socket.on("error", (error) => {
+    console.error("Socket error:", error);
+  });
+});
+
+httpServer.listen(process.env.PORT, () => {
   console.log("listening on port " + process.env.PORT);
 });
