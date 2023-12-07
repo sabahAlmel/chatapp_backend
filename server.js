e590e1f09985435b9da840ad41d692d4244cad9c
Hasan Deeb <sektories7@gmail.com>
2023-12-07 09:20:40 +0200

    finished

--- server.js
+++ server.js
@@ -13,14 +13,24 @@ import { createServer } from "http";
 
 const httpServer = createServer(app);
 
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+  cors: { origin: "http://localhost:3000" },
+});
+
 io.on("connection", (socket) => {
-  socket.emit("connect", { message: "a new client" });
-  socket.on("error", (error) => {
-    console.error("Socket error:", error);
+  console.log(socket.id);
+
+  socket.on("join_room", (data) => {
+    console.log(`room: ${data.room}`);
+  });
+
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.broadcast.emit("recieve_message", {
+      text: data.text,
+      sender: "User 1",
+    });
   });
 });
 
-httpServer.listen(process.env.PORT, () => {
-  console.log("listening on port " + process.env.PORT);
-});
+httpServer.listen(process.env.PORT);
